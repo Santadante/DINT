6ef6bfd6409bb6053b64193eda8d8967a38f3a00
Santadante <49271860+Santadante@users.noreply.github.com>
2020-10-16 19:37:29 +0200

    Add files via upload

---
+++ _01_AdivinarNumero/_01_AdivinarNumero/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_AdivinarNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Random r = new Random();
+        private int n = r.Next(0, 101);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void comprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroIntroducido = Convert.ToInt32(numeroIntroducidoTextBox.Text);
+            if (numeroIntroducido == n)
+                MensajeTextBlock.Text = "¡¡Has acertado!!";
+            else if (numeroIntroducido > n)
+                MensajeTextBlock.Text = "Te has pasado";
+            else
+                MensajeTextBlock.Text = "Te has quedado corto";
+        }
+
+        private void reiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            n = r.Next(0, 101);
+            MensajeTextBlock.Text = "";
+            numeroIntroducidoTextBox.Text = "";
+        }
+    }
+}
