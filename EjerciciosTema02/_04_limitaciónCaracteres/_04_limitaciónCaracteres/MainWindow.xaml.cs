97d8ab54eff062be315383f317b0fe10f0ba1e3d
abc <abc@example.com>
2020-10-16 19:48:43 +0200

    archivos

---
+++ EjerciciosTema02/_04_limitaciónCaracteres/_04_limitaciónCaracteres/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04_limitaciónCaracteres
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Color colorMedio = (Color)ColorConverter.ConvertFromString("#FF992DC1");
+            Color colorFinal = (Color)ColorConverter.ConvertFromString("#FFFF0068");
+            if (textoTextBox.Text.Length == textoTextBox.MaxLength)
+            {
+                contadorTextBlock.Foreground = new System.Windows.Media.SolidColorBrush(colorFinal);
+                textoTextBox.IsReadOnly = true;
+            }
+                
+            contadorTextBlock.Text = textoTextBox.Text.Length.ToString() + " / 140";
+            if (textoTextBox.Text.Length == textoTextBox.MaxLength / 2)
+                contadorTextBlock.Foreground = new System.Windows.Media.SolidColorBrush(colorMedio);
+        }
+    }
+}
