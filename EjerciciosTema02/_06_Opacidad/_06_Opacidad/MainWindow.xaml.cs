05edb521d5b8535a1b51e478ff6c15b5c487be66
abc <abc@example.com>
2020-10-20 15:36:29 +0200

    actualizando

---
+++ EjerciciosTema02/_06_Opacidad/_06_Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace _06_Opacidad
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cuatroImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image i = (Image)sender;
+            i.Opacity = 1.0;
+        }
+
+        private void cuatroImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image i = (Image)sender;
+            i.Opacity = 0.5;
+        }
+    }
+}
---
+++ EjerciciosTema02/_08_CuadroDeTexto/_08_CuadroDeTexto/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace _08_CuadroDeTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void nombreTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1 && ayudaNombreTextBlock.Text == "")
+                ayudaNombreTextBlock.Text = "Nombre del cliente";
+            else if(e.Key == Key.F1)
+                ayudaNombreTextBlock.Text = "";
+        }
+
+        private void ApellidosTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1 && ayudaApellidosTextBlock.Text == "")
+                ayudaApellidosTextBlock.Text = "Apellidos del cliente";
+            else if(e.Key == Key.F1)
+                ayudaApellidosTextBlock.Text = "";
+        }
+
+        private void edadTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2 && !Regex.IsMatch(edadTextBox.Text, "^[0-9]+$"))
+                comprobarEdadTextBlock.Text = "Edad Incorrecta";
+            else if(e.Key == Key.F2)
+                comprobarEdadTextBlock.Text = "";
+        }
+    }
+}
